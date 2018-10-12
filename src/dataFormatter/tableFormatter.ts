d854a652ad9f84f2d9cefb696a26249f017ee19c
Kacper Kula <kulak@kulak.at>
2018-10-12 11:45:26 +0200

    Fixed table formatter to pad the values according to the column.

--- src/dataFormatter/tableFormatter.ts
+++ src/dataFormatter/tableFormatter.ts
@@ -2,6 +2,22 @@ import { IDataFormatter } from './IDataFormatter';
 import { TableGeneratorResult } from '../tableGenerator/TableGenerator';
 const os = require('os');
 
+function padTable(tab: any[][]): string[][] {
+    // assuming all the rows have the same length
+    // TODO: this function mutates table, should probably create a copy instead
+    for(let i=0;i<tab[0].length;i++) {
+        let maxLen = 0;
+        for(let j=0;j<tab.length;j++) {
+            tab[j][i] = tab[j][i].toString();
+            maxLen = Math.max(maxLen, tab[j][i].length);
+        }
+        for(let j=0;j<tab.length;j++) {
+            tab[j][i] = tab[j][i].padStart(maxLen, ' ');
+        }
+    }
+    return tab;
+}
+
 export class TableFormatter implements IDataFormatter {
     format(table: TableGeneratorResult): string {
         const res = [
@@ -9,6 +25,6 @@ export class TableFormatter implements IDataFormatter {
             ...(table.values.map((r,i) => [table.rows[i], ...r]))
         ];
 
-        return res.map(row => row.join(' | ')).join(os.EOL);
+        return padTable(res).map(row => row.join(' | ')).join(os.EOL);
     }
 }
